d2b6c5a6a5214b4eb0074ceb1496a9e2eca6ed41
MarvinWaecker <marvin.waecker@icloud.com>
2025-05-20 11:00:16 +0200

    fixed gpt api

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
-# from openai_api import get_response
+from openai_api import get_response
 # from mistral_api import get_response
-from deepseek_api import get_response
+# from deepseek_api import get_response
 from app import create_message
 import streamlit as st
 
-API_KEY = st.secrets['deepseek']['key']
-AI_MODEL =  "deepseek-chat" # "gpt-3.5-turbo" # "mistral-large-latest"
+API_KEY = st.secrets['openai']['key'] # st.secrets['deepseek']['key']
+AI_MODEL =   "gpt-3.5-turbo" # "mistral-large-latest" # "deepseek-chat"
 
 car_model = "BMW 218i Cabrio"
 car_year = "2021"
